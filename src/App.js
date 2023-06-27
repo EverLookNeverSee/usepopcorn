6accb627194a047009359525e8bbf60ff25285a9
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-06-27 18:36:34 +0330

    Prop drilling

--- src/App.js
+++ src/App.js
@@ -50,20 +50,21 @@ const tempWatchedData = [
 const average = arr => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+	const [movies, setMovies] = useState(tempMovieData);
 	return (
 		<>
-			<NavBar />
-			<Main />
+			<NavBar movies={movies} />
+			<Main movies={movies} />
 		</>
 	);
 }
 
-function NavBar() {
+function NavBar({movies}) {
 	return (
 		<nav className="nav-bar">
 			<Logo />
 			<Search />
-			<NumResults />
+			<NumResults movies={movies} />
 		</nav>
 	);
 }
@@ -90,24 +91,24 @@ function Search() {
 	);
 }
 
-function NumResults() {
+function NumResults({movies}) {
 	return (
 		<p className="num-results">
-			Found <strong>X</strong> results
+			Found <strong>{movies.length}</strong> results
 		</p>
 	);
 }
 
-function Main() {
+function Main({movies}) {
 	return (
 		<main className="main">
-			<ListBox />
+			<ListBox movies={movies} />
 			<WatchedBox />
 		</main>
 	);
 }
 
-function ListBox() {
+function ListBox({movies}) {
 	const [isOpen1, setIsOpen1] = useState(true);
 
 	return (
@@ -115,13 +116,12 @@ function ListBox() {
 			<button className="btn-toggle" onClick={() => setIsOpen1(open => !open)}>
 				{isOpen1 ? "–" : "+"}
 			</button>
-			{isOpen1 && <MoviesList />}
+			{isOpen1 && <MoviesList movies={movies} />}
 		</div>
 	);
 }
 
-function MoviesList() {
-	const [movies, setMovies] = useState(tempMovieData);
+function MoviesList({movies}) {
 	return (
 		<ul className="list">
 			{movies?.map(movie => (
