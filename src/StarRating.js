ce9e5dd570402d9c7af6bba77bb0804d6776edd6
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-07-01 12:36:47 +0330

    Completing the implementation of StarRating component

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,15 +1,32 @@
+import {useState} from "react";
+
 const containerStyle = {display: "flex", alignItems: "center", gap: "16px"};
-const starContainerStyle = {display: "flex", gap: "4px"};
+const starContainerStyle = {display: "flex"};
 const textStyle = {lineHeight: "1", margin: "0"};
+
 export default function StarRating({maxRating = 5}) {
+	const [rating, setRating] = useState(0);
+	const [tempRating, setTempRating] = useState(0);
+
+	function handleRating(rating) {
+		setRating(rating);
+	}
 	return (
 		<div style={containerStyle}>
 			<div style={starContainerStyle}>
 				{Array.from({length: maxRating}, (_, i) => (
-					<span style={textStyle}>S{i + 1}</span>
+					<span style={textStyle}>
+						<Star
+							key={i}
+							onRate={() => handleRating(i + 1)}
+							onHoverIn={() => setTempRating(i + 1)}
+							onHoverOut={() => setTempRating(0)}
+							full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+						/>
+					</span>
 				))}
 			</div>
-			<p>10</p>
+			<p style={textStyle}>{tempRating || rating || ""}</p>
 		</div>
 	);
 }
