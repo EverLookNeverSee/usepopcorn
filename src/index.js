026d8352b02713e51d6b48bcb5c5cf283432f0ca
Milad Sadeghi DM <MiladSadeghiDM@Gmail.com>
2023-07-11 10:40:59 +0330

    Removing unused codes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppV1 from "./App-v1";
-import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<AppV1 />
-		{/*<StarRating*/}
-		{/*	maxRating={5}*/}
-		{/*	messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}*/}
-		{/*	defaultRating={3}*/}
-		{/*/>*/}
 	</React.StrictMode>,
 );
