6e0c840f0329ee30efd2e54d0f1e027efe1c8a16
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-07-17 15:14:46 +0330

    Using useRef hook to focus on input element

--- src/App-v2.js
+++ src/App-v2.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import StarRating from "./StarRating";
 
 const average = arr => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -135,6 +135,24 @@ function Logo() {
 }
 
 function Search({query, setQuery}) {
+	// useEffect(() => {
+	// 	const el = document.querySelector(".search");
+	// 	el.focus();
+	// }, []);
+
+	const inputEl = useRef(null);
+	useEffect(() => {
+		function callback(e) {
+			if (document.activeElement === inputEl.current) return;
+			if (e.code === "Enter") {
+				inputEl.current.focus();
+				setQuery("");
+			}
+		}
+		document.addEventListener("keydown", callback);
+		return () => document.addEventListener("keydown", callback);
+	}, [setQuery]);
+
 	return (
 		<input
 			className="search"
@@ -142,6 +160,7 @@ function Search({query, setQuery}) {
 			placeholder="Search movies..."
 			value={query}
 			onChange={e => setQuery(e.target.value)}
+			ref={inputEl}
 		/>
 	);
 }
